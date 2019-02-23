892079dc9bbd024eb8bf10c5c36a386683eab0cc
Alisher778 <alisher.musurmonov89@gmail.com>
2019-02-23 16:21:40 +0500

    added error boundry page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import User from './components/User'
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
+      <ErrorBoundary>
         <div className="App">
           <h1>Hello react</h1>
           <User />
         </div>
+      </ErrorBoundary>
     );
   }
 }
