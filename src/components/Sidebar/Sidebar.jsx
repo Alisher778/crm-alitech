cc016fb0eb30626428e40dc3720c4abe445dad7f
Alisher778 <alisher.musurmonov89@gmail.com>
2019-02-25 19:27:45 +0500

    added menu close button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import cssClasses from './Sidebar.css';
+import logo from '../../assets/imgs/logo.svg';
 
 const sidebar = (props) => {
     return (
         <aside className={cssClasses.SidebarLinks}>
+            <div className={cssClasses.CloseBtn}>X</div>
+            <a href="#"><img src={logo} className={cssClasses.Logo} alt="Company Logo"/></a>
             <a href="#">Dashboar</a>
             <a href="#">Students</a>
             <a href="#">Courses</a>
