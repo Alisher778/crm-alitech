8f95ba1d75d15d0f3ec144df5134f427d1c0f230
Alisher778 <alisher.musurmonov89@gmail.com>
2019-03-10 22:50:23 +0500

    added students profile contact information

--- src/components/Students/StudentProfile/StudentProfile.jsx
+++ src/components/Students/StudentProfile/StudentProfile.jsx
@@ -11,12 +11,32 @@ class StudentProfile extends Component {
     render() {
         const userId = this.props.match.params.id;
         const currentUser = this.props.students.find(item => item.id === userId);
-        const {email, phone, firstName, lastName, createdAt} = currentUser;
+        const {email, phone, firstName, lastName, createdAt, address} = currentUser;
         console.log('[cureentUser] =>', currentUser)
         return (
             <div>
                 <div className={cssClasses.LeftContainer}>
-                    <img src={avatar} alt={firstName}/>
+                    <img src={avatar} alt={firstName} className={cssClasses.Avatar}/>
+                    <h3>{firstName + ' ' + lastName}</h3>
+                    <div className="ContactInformation">
+                        <h5>Contact Information</h5>
+                        <div>
+                            <i>Phone Number:</i>
+                            <div><a href={'tel:+'+phone}>+{phone}</a></div>
+                        </div>
+                        <div>
+                            <i>Eamil Address:</i>
+                            <div><a href={'mailto:'+email}>{email}</a></div>
+                        </div>
+                        <div>
+                            <i>Address:</i>
+                            <div>{address}</div>
+                        </div>
+                        <div>
+                            <i>Joined School:</i>
+                            <div>{createdAt}</div>
+                        </div>
+                    </div>
                 </div>
                 <div className={cssClasses.RightContainer}></div>
             </div>
