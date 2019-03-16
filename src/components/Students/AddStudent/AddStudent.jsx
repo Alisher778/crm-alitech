ae5fcdabb10663d0c2b8130cf1349622edb5a27e
Alisher778 <alisher.musurmonov89@gmail.com>
2019-03-16 18:11:21 +0500

    added active course id

--- src/components/Students/AddStudent/AddStudent.jsx
+++ src/components/Students/AddStudent/AddStudent.jsx
@@ -3,31 +3,48 @@ import { Link } from 'react-router-dom';
 import firebase from '../../../config/firebaseConfig';
 import cssClasses from './AddStudent.css';
 const db = firebase.firestore().collection('students');
+const courseDB = firebase.firestore().collection('courses');
 
 class AddStudent extends Component {
-    state = { firstName: '', 
-              lastName: '', 
-              phone: '', 
-              email: '', 
-              address: '',
-              createdAt: Date.now(), 
-              updatedAt: Date.now()
+    state = { info : {
+                    firstName: '', 
+                    lastName: '', 
+                    phone: '', 
+                    email: '', 
+                    address: '',
+                    status: 'active',
+                    createdAt: Date.now(), 
+                    updatedAt: Date.now(),
+                    courseId: '',
+                },   
+              courseList: []
             }
-    
+    componentDidMount() {
+        courseDB.where('status', '==', 'active').get().then(snapShots => {
+            let coursesArray = [];
+            snapShots.docs.forEach(doc => {
+                coursesArray.push({id: doc.id, ...doc.data()})
+            });
+            return this.setState({courseList: coursesArray});
+        })
+    }
+
     userInputHandler = (e) => {
         e.preventDefault();
         const { name, value } = e.target;
-        this.setState({[name]: value});
+        this.setState({info:{...this.state.info, [name]: value}});
     }
     formSubmitHandler = (e) => {
         e.preventDefault();
-        db.add({...this.state})
+        
+        db.add({...this.state.info})
             .then(res => {
                 console.log('[Successfully saved data]')
             })
             .catch(err => console.log(err))
     }
     render() {
+        console.log(this.state)
         return(
             <div>
                 <div className={cssClasses.ButtonWrapper}>
@@ -67,6 +84,29 @@ class AddStudent extends Component {
                             placeholder="Address"
                             onChange={(event) => this.userInputHandler(event)}
                         />
+                        <select 
+                            name="status" 
+                            value={this.state.status} 
+                            onChange={(event) => this.userInputHandler(event)}
+                        > 
+                            <option value="active">Students Status</option>
+                            <option value="active">Active</option>
+                            <option value="inactive">Inactive</option>
+                            <option value="pending">Pending</option>
+                            <option value="canceled">Canceled</option>
+                        </select>
+                        <select 
+                            name="courseId" 
+                            value={this.state.courseId} 
+                            onChange={(event) => this.userInputHandler(event)}
+                            required
+                        >   <option value="">Choose The Course</option>
+                            {this.state.courseList.map(item => {
+                                return (
+                                    <option value={item.id} key={item.id}>{item.title}</option>
+                                );
+                            })}
+                        </select>
                         <button onClick={(event) => this.formSubmitHandler(event)}>Add Student</button>
                     </div>
                 </form>
